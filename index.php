e7f796f050656f049c2a724fecfb22976042e903
Bryan <bryches@gmail.com>
2017-03-26 17:53:23 -0400

    Added user input functions

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<?php include 'main.php'; ?>
+
 <html>
 <head>
-	<title>Queued</title>
+	<title>QDesk</title>
 
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
@@ -30,15 +32,15 @@
 		<div class='row'><div class='col-md-12 spacer-md'></div></div>
 		
 		<div class='row'><div class='col-md-6 col-md-offset-3'>
-			<form>
+			<form method='post'>
 				<div class='form-group'>
 					<label>Requestor</label>
-					<input type='text' class='form-control' placeholder='Your name'>
+					<input name='name' type='text' class='form-control' placeholder='Your name'>
 				</div>
 
 				<div class='form-group'>
 					<label>Project</label>
-					<select class='form-control'>
+					<select name='project' class='form-control'>
 						<option>26Ah NMC</option>
 						<option>20Ah VDA</option>
 						<option>General</option>
@@ -47,25 +49,25 @@
 
 				<div class='form-group'>
 					<label>Title</label>
-					<input type='text' class='form-control' placeholder='Short description of request'>
+					<input name='title' type='text' class='form-control' placeholder='Short description of request'>
 				</div>
 
 				<div class='form-group'>
 					<label>When do you need it?</label>
-					<input type='text' class='form-control' placeholder='Accepts most date formats. You can also type "2 weeks", "next Monday", etc.'>
+					<input name='due' type='text' class='form-control' placeholder='Accepts most date formats. You can also type "2 weeks", "next Monday", etc.'>
 				</div>
 
 				<div class='form-group'>
 					<label>Background</label>
-					<textarea class='form-control' rows='5' placeholder='Describe the request background and deliverables using clear and consise language.'></textarea>
+					<textarea name='background' class='form-control' rows='5' placeholder='Describe the request background and deliverables using clear and consise language.'></textarea>
 				</div>
 
 				<div class='form-group'>
 					<label>Deliverables</label>
-					<textarea class='form-control' rows='5' placeholder='Describe the request background and deliverables using clear and consise language.'></textarea>
+					<textarea name='deliverables' class='form-control' rows='5' placeholder='Describe the request background and deliverables using clear and consise language.'></textarea>
 				</div>
 
-				<button type='submit' class='btn btn-primary'>Submit</button>
+				<input name='submit' type='submit' class='btn btn-primary' value='Submit'>
 			</form>
 		</div></div>
 
---
+++ input.php
@@ -0,0 +1,78 @@
+<?php
+
+function processInput() {
+	if(isset($_POST['submit'])) {
+		$inputs = getInputs();
+		$errors = getInputErrors($inputs);
+
+		return ('inputs' => $inputs, 'errors' => $errors);
+	}
+}
+
+
+
+
+
+function getInputs() {
+	if(isset($_POST['submit'])) {
+		return [
+			'name'         => $_POST['name'],
+			'project'      => $_POST['project'],
+			'title'        => $_POST['title'],
+			'due'          => $_POST['due'],
+			'background'   => $_POST['background'],
+			'deliverables' => $_POST['deliverables']
+		];
+	}
+	else { return false; }
+}
+
+
+
+
+
+function getInputErrors($inputs) {
+	$err = [];
+
+	$err['name']         = getTextErrors('name', $inputs['name'], 50, true);
+	$err['title']        = getTextErrors('title', $inputs['title'], 150, false);
+	$err['background']   = getTextErrors('background', $inputs['background'], 1000, false);
+	$err['deliverables'] = getTextErrors('deliverables', $inputs['deliverables'], 1000, false);
+
+	if($input['date'] != '') {
+		if(strtotime($input['due'])) {
+			//Valid date
+		}
+		else { $err['due'] = 'Invalid date'; }
+	}
+	else { $err['due'] = 'Due date cannot be blank'; }
+
+	foreach($err as $key=>$msg) {
+		if(!$msg) { unset($err[$key]); }
+	}
+
+	if(count($err) > 0) {
+		return $err;
+	}
+	else { return false; }
+}
+
+
+
+
+
+function getTextErrors($key, $text, $max, $alpha) {
+	if($text != '') {
+		if(strlen($text) < $max) {
+			if($alpha) {
+				if(ctype_alpha(str_replace(' ', '', $text))) {
+					return false;
+				}
+				else { return ucfirst($key) . " can only contain letters and spaces"; }
+			}
+			else { return false; }
+		}
+		else { return ucfirst($key) . " must be less than $max characters"; }
+	}
+	else { return ucfirst($key) . " cannot be blank"; }
+}
