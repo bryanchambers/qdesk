1c4cfd6304514278c76641a150334237c3986f16
Bryan <bryches@gmail.com>
2017-03-29 21:18:31 -0400

    More work on database functions

--- db.php
+++ db.php
@@ -18,11 +18,12 @@ function dbConnect() {
 function createTableRequests($db) {
 	$query = "CREATE TABLE requests(
 				id INT PRIMARY KEY AUTO_INCREMENT,
-				status_id INT, 
-				client VARCHAR(100),
+				status_id INT NOT NULL, 
+				client VARCHAR(100) NOT NULL,
+				agent VARCHAR(100),
 				project_id INT,
-				title VARCHAR(255),
-				opened INT,
+				title VARCHAR(255) NOT NULL,
+				opened INT NOT NULL,
 				requested_due_date INT,
 				final_due_date INT,
 				closed INT,
@@ -52,29 +53,83 @@ function createTableProjects($db) {
 }
 
 
-function createRequest() {}
 
-function changeStatus() {}
 
-function changeDueDate() {}
+function createRequest($db, $client, $project_id, $title, $requested_due_date, $background, $deliverables) {
+	$query = "INSERT INTO requests(status_id, client, project_id, title, opened, requested_due_date, background, deliverables)
+			  VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
-function deleteRequest() {}
+	$stmt = $db->prepare($query);
+	$stmt->bind_param('isisiiss', $status_id, $client, $project_id, $title, $opened, $requested_due_date, $background, $deliverables);
 
-function getRequests() {}
+	$status_id = 1;
+	$opened = time();
+
+	$stmt->execute();
+}
+
+
+
+function changeStatus($db, $id, $status_id) {
+	$query = "UPDATE requests SET status_id = $status_id WHERE id = $id LIMIT 1";
+	$result = $db->query($query);
+}
+
+
+function changeDueDate($db, $id, $date) {
+	$query = "UPDATE requests SET final_due_date = $date WHERE id = $id LIMIT 1";
+	$result = $db->query($query);
+}
+
+
+
+function deleteRequest($db, $id) {
+	$query = "DELETE FROM requests WHERE id = $id LIMIT 1";
+	$result = $db->query($query);
+}
+
+
+
+function getRequests() {
+	$query = "SELECT requests.id, requests.status_id, statuses.name, statuses.css_class, 
+					 requests.client, requests.project_id, projects.name, project.code, 
+					 requests.title, requests.opened, requests.requested_due_date, 
+					 requests.final_due_date, requests.closed
+				FROM requests
+				JOIN statuses ON requests.status_id = statuses.id
+				JOIN projects ON requests.project_id = projects.id";
+	$result = $db->query($query);
+}
+
+
 
 function searchRequests() {}
 
+
+
 function getOneRequest() {}
+
+
 
 function createProject() {}
 
+
+
 function deleteProject() {}
+
+
 
 function getProjects() {}
 
+
+
 function createStatus() {}
 
+
+
 function deleteStatus() {}
+
+
 
 function getStatuses() {}
 
